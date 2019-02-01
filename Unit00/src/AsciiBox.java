925491d520c20a160a2a6ac0cee9bd64e79b9e6e
Shrey Sambhwani <shreysambhwani@gmail.com>
2019-02-01 10:23:29 -0800

    Added files for Unit0

---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Shrey Sambhwani
+//Date - 1/31/19
+//Class - APCSA
+//Lab  - AsciiBox
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Shrey Sambhwani \t  1/31/19 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+}
---
+++ Unit00/src/Variables.java
@@ -0,0 +1,67 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = -127;
+	    int  intOne = 127;
+		long longOne = 12733;
+
+
+		//decimal variables
+		float floatOne = (float)67.44;
+		double doubleOne = (double)67.4438383;
+
+		//other integer types
+		char charOne = 7;
+
+		//other types
+		boolean booleanOne = true;
+		String stringOne ="helloworld";
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+		System.out.println("*        real types          *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("*        othervinteger types          *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*        other types          *");
+		System.out.println("*booleanOne = "+booleanOne+"\t\t*");
+		System.out.println("*stringOne = "+stringOne+"\t\t*");
+		System.out.println("/////////////////////////////////");
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
