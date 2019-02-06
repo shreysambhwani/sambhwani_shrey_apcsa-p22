c7bcb232e37e3fa0e43698b13875d501a7d32193
Shrey Sambhwani <shreysambhwani@gmail.com>
2019-02-05 22:57:13 -0800

    FInish Unit 2 and Added Unit 3 Files

---
+++ Unit02/src/Trap.java
@@ -0,0 +1,13 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return (double)((base1 + base2)*.5*height );
+	}
+}
